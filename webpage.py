06bf328f675b2b0b523bc14d4ec4757ebb561b96
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-07-11 23:36:00 -0500

    Basic Layout Update

--- webpage.py
+++ webpage.py
@@ -1,10 +1,42 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
-app.secret_key = "39fds032_**(32"
+app.secret_key = "3546*&^*dsflk1234"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/vehinventory")
+def vehicle_inventory():
+    return render_template("vehicle_inventory.html")
+
+
+@app.route("/modinventory")
+def modify_inventory():
+    return render_template("modify_inventory.html")
+
+@app.route("/modsales")
+def modify_sales():
+    return render_template("modify_sales.html")
+
+@app.route("/searchsales")
+def search_sales():
+    return render_template("search_sales.html")
+
+
+@app.route("/testdrive")
+def test_drive():
+    return render_template("test_drive.html")
+
+@app.route("/cfproject")
+def cf_projection():
+    return render_template("cf_projection.html")
 
 
 
+'''
 @app.route("/login", methods = ["POST","GET"])
 def login():
     if request.method == "POST":
@@ -25,7 +57,7 @@ def user():
 @app.route("/logout")
 def logout():
     session.pop("user",None)
-
+'''
 
 
 if __name__ == "__main__":
